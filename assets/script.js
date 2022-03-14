cdd4efc8105bcc74c5fe75802a9bfc8e66391085
Raissa-Cardoso <raissa_cardoso@yahoo.com.br>
2022-03-14 02:36:33 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,16 @@
 let i=0        
 const imagens = document.querySelectorAll("[wm-slider] > p")      
-function clicou(){ 
-    console.log(i, imagens.length)                      
+function clicou(){          
     let atual= imagens[i]
     atual.style.display="none";
     let proxima=0                   
     if(i===imagens.length-1){
         proxima=imagens[0]                
-        i=0                
-        console.log("if")                          
+        i=0             
     } else {
         proxima=imagens[i+1]               
         i++
-        console.log("else")
     }
         proxima.style.display="block"                
         proxima.classList.add('mover');                  
-
 }
